1a407693faee0be2f7833dd4515d9241d64d58f6
Joel Santos <joeljesussantos50@gmail.com>
2021-05-19 17:06:08 +0100

    Added docstrings and altered some parameters.

--- bio_converter.py
+++ bio_converter.py
@@ -55,19 +55,34 @@ end;
 import sys
 
 
-def handle_errors(text='', filename=''):
+def handle_errors(filename, text=''):
+    """A function to handle some errors and warnings to a file and to the stderr.
+
+    Args:
+        filename (str): filename you want to output the text.
+        text (str, optional): text you want to print in the stderr and save in the file. Defaults to ''.
+    """
     print(text, file = sys.stderr)
     sys.stderr = open(filename, 'w')
     print(text, file = sys.stderr)
     sys.stderr = sys.__stderr__
 
-def open_fasta(fasta_file=''):
+def open_fasta(fasta_file):
+    """A function that returns the read lines of a fasta file.
+
+    Args:
+        fasta_file (str): the name of your fasta file.
+
+    Returns:
+        list of str: If successfully executed returns a list with all the lines of the fasta file.
+        Otherwise prints a error mensage and creates a error file.
+    """
     try:
         with open(fasta_file, 'r') as fasta:
             return fasta.readlines()
     except FileNotFoundError:
         error_file = fasta_file.rsplit('.', 1)[0] + "_errors.txt"
-        handle_errors("We cannot find the file. Verify if you have typed the file name correctly.", error_file)
+        handle_errors(error_file, "We cannot find the file. Verify if you have typed the file name correctly.")
         exit()
 
 # quantas condições????
@@ -76,7 +91,15 @@ def open_fasta(fasta_file=''):
 # sem >
 
 
-def max_length(lines=''):
+def max_length(lines):
+    """Function that returns the greatest length of a list of lines/strings.
+
+    Args:
+        lines (list of str): A list of lines/strings.
+
+    Returns:
+        int: The greatest length of the element of the list 'lines'.
+    """
     max_len = 0
     for line in lines:
         if max_len < len(line):
@@ -84,7 +107,16 @@ def max_length(lines=''):
     return max_len
 
 
-def nchar(lines=''):
+def nchar(lines):
+    """Function to calculate the length of each sequence of a fasta file to put that value in the variable
+    NCHAR of the correspondent nexus file.
+
+    Args:
+        lines (list of str): A list with all the lines of the fasta file.
+
+    Returns:
+        int: The length of each sequence.
+    """
     count = 0
     length = 0
     for line in lines:
@@ -98,7 +130,17 @@ def nchar(lines=''):
 
 
 
-def write_nexus(nex_file='', lines='', lines_formated=False, name_lines_formated='', ntax = 0, nchar=0):    
+def write_nexus(nex_file, lines, ntax, nchar, lines_formatted=False, name_lines_formatted=''):
+    """A function to write a nexus file via a fasta file.
+
+    Args:
+        nex_file (str): name of the nexus file.
+        lines (list of str): a list with all the lines of the fasta file (to convert to the nexus format).
+        ntax (int): the value of ntax.
+        nchar (int): the value of nchar.
+        lines_formatted (bool, optional): If true the name lines will be formatted according to name_lines_formatted. Defaults to False.
+        name_lines_formatted (list of str, optional): your name sequences with your formatation. Defaults to ''.
+    """    
     with open(nex_file, 'w') as nexus:
         nexus.write(f"""#NEXUS
 
@@ -107,12 +149,12 @@ DIMENSIONS NTAX={ntax} NCHAR={nchar};
 FORMAT DATATYPE=DNA MISSING=N GAP=-;
 MATRIX""")
 
-        if lines_formated:
+        if lines_formatted:
             counter = 0
         for line in lines:
             if '>' in line:
-                if lines_formated:
-                    nexus.write('\n' + name_lines_formated[counter] + '  ')
+                if lines_formatted:
+                    nexus.write('\n' + name_lines_formatted[counter] + '  ')
                     counter += 1
                 else:
                     nexus.write('\n' + line[:99].strip().replace(' ', '_').replace('>', ' ') + '  ')
@@ -141,13 +183,13 @@ if __name__ == '__main__':
     max_len_lines = max_length(name_lines)
     if max_len_lines > 99:
         error_file = sys.argv[1].rsplit('.', 1)[0] + "_errors.txt"
-        handle_errors("WARNING: some of your data names had passed the character limit and they'll be limited to 99 characters.", error_file)
+        handle_errors(error_file, "WARNING: some of your data names had passed the character limit and they'll be limited to 99 characters.")
     max_len_lines = max_len_lines if max_len_lines < 100 else 99
-    name_lines_formated = tuple(map(lambda line='': ' '*(max_len_lines-len(line)) + line.strip()[:100].replace(' ', '_').replace('>', ' '), name_lines))
+    name_lines_formatted = tuple(map(lambda line='': ' '*(max_len_lines-len(line)) + line.strip()[:100].replace(' ', '_').replace('>', ' '), name_lines))
     #  first_seq....
     # second seq....
     #  third_seq....
     make_nexus = lambda fasta_file='': fasta_file.split('.')[0] + '.nex'
     nexus = make_nexus(sys.argv[1])
-    write_nexus(nexus, lines, True, name_lines_formated, ntax=len(name_lines), nchar=nchar(lines))
+    write_nexus(nexus, lines, ntax=len(name_lines), nchar=nchar(lines), lines_formatted=True, name_lines_formatted=name_lines_formatted)
 
